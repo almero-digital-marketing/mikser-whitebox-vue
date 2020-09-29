88fd5fd2d222fcefe9405fb59df7a3ebc37cdc11
Dimitar Kolev-Dick <dick@ingbg.com>
2020-09-29 09:20:49 +0300

    Better mackine id

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const os = require('os')
 const { machineIdSync } = require('node-machine-id')
 
 if (process.env.NODE_ENV == 'development') {
-    const machineId = machineIdSync() + '_' + os.hostname()
+	const machineId = machineIdSync() + '_' + os.hostname() + '_' + os.userInfo().username
     process.env.VUE_APP_WHITEBOX_CONTEXT = machineId
 }
 
